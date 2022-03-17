3f32b6c2676f4e191070b1d5a52209dd2fdea0a9
reinhard.seiler <reinhard.seiler@n26.com>
2022-03-17 15:20:27 +0100

    Fix test

--- src/main/kotlin/de/tech26/supermarket/infrastructure/driver/CheckoutController.kt
+++ src/main/kotlin/de/tech26/supermarket/infrastructure/driver/CheckoutController.kt
@@ -1,24 +1,24 @@
 package de.tech26.supermarket.infrastructure.driver
 
+import de.tech26.supermarket.domain.usecase.CheckoutUseCase
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class CheckoutController {
+class CheckoutController(
+    private val checkoutUseCase: CheckoutUseCase
+) {
 
     @PostMapping("/checkout")
-    fun checkout(@RequestBody request: HttpCheckoutRequest): ResponseEntity<HttpCheckoutResponse> {
-        if(request.skus.isEmpty()) {
-            return ResponseEntity(HttpStatus.NO_CONTENT)
-        }
-        return ResponseEntity(HttpCheckoutResponse(18.0), HttpStatus.CREATED)
-    }
-
+    fun checkout(@RequestBody request: HttpCheckoutRequest): ResponseEntity<HttpCheckoutResponse> =
+        if (request.skus.isEmpty()) ResponseEntity(NO_CONTENT)
+        else ResponseEntity(HttpCheckoutResponse(checkoutUseCase(request.skus)), CREATED)
 }
 
-data class HttpCheckoutRequest (val skus: List<String>)
+data class HttpCheckoutRequest(val skus: List<String>)
 
 data class HttpCheckoutResponse(val total: Double)
