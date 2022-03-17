9dfe6a26e3d2d4915b3a272cec19ca17d368c2f3
iakov.burtsev <iakov.burtsev@n26.com>
2022-03-17 15:10:31 +0100

    Added CheckoutUseCase.kt

---
+++ src/main/kotlin/de/tech26/supermarket/domain/usecase/CheckoutUseCase.kt
@@ -0,0 +1,10 @@
+package de.tech26.supermarket.domain.usecase
+
+import org.springframework.stereotype.Service
+
+@Service
+class CheckoutUseCase {
+    operator fun invoke(input: List<String>): Double {
+        TODO()
+    }
+}
--- src/test/kotlin/de/tech26/supermarket/infrastructure/driver/CheckoutControllerTest.kt
+++ src/test/kotlin/de/tech26/supermarket/infrastructure/driver/CheckoutControllerTest.kt
@@ -1,5 +1,9 @@
 package de.tech26.supermarket.infrastructure.driver
 
+import com.ninjasquad.springmockk.MockkBean
+import de.tech26.supermarket.domain.usecase.CheckoutUseCase
+import io.mockk.every
+import io.mockk.verify
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,11 +15,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @Tag("integration")
-@WebMvcTest(value = [ChangeMeController::class])
-class ChangeMeControllerTest(@Autowired private val mockMvc: MockMvc) {
+@WebMvcTest(value = [CheckoutController::class])
+class CheckoutControllerTest(@Autowired private val mockMvc: MockMvc) {
 
-//    @MockkBean(relaxed = true)
-//    private lateinit var myDependency: MyDependency
+    @MockkBean
+    private lateinit var checkoutUseCase: CheckoutUseCase
 
     @Test
     fun `should not calculate anything if the cart is empty of products`() {
@@ -29,19 +33,25 @@ class ChangeMeControllerTest(@Autowired private val mockMvc: MockMvc) {
 
     @Test
     fun `should checkout a cart of products`() {
+        val itemsInTheCart = listOf("A", "B", "A", "B", "A", "A", "A")
+        every { checkoutUseCase(itemsInTheCart) } returns 18.0
+
         val result = mockMvc.perform(
             post("/checkout").content("""{ "skus": ["A","B","A","B","A","A","A"] }""")
                 .contentType(MediaType.APPLICATION_JSON)
         )
 
+        verify { checkoutUseCase.invoke(itemsInTheCart) }
         result
-            .andExpect(content().json(
-                """
+            .andExpect(
+                content().json(
+                    """
                     {
                         "total": 18.00
                     }
                 """.trimIndent()
-            ))
+                )
+            )
             .andExpect(status().isCreated)
     }
 }
