7fb23f5abec3e346c91fc5717024dae63f3c5ddb
iakov.burtsev <iakov.burtsev@n26.com>
2022-03-18 10:35:43 +0100

    Add checkout calculation not empty cart test

--- src/main/kotlin/de/tech26/supermarket/domain/usecase/CheckoutUseCase.kt
+++ src/main/kotlin/de/tech26/supermarket/domain/usecase/CheckoutUseCase.kt
@@ -1,13 +1,15 @@
 package de.tech26.supermarket.domain.usecase
 
 import arrow.core.Either
+import arrow.core.right
 import de.tech26.supermarket.domain.ItemsNotFoundError
-import org.springframework.stereotype.Service
 import java.math.BigDecimal
+import org.springframework.stereotype.Service
 
 @Service
 class CheckoutUseCase {
+
     operator fun invoke(input: List<String>): Either<ItemsNotFoundError, BigDecimal> {
-        TODO()
+        return BigDecimal.ZERO.right()
     }
 }
--- src/test/kotlin/de/tech26/supermarket/domain/usecase/CheckoutUseCaseTest.kt
+++ src/test/kotlin/de/tech26/supermarket/domain/usecase/CheckoutUseCaseTest.kt
@@ -1,18 +1,32 @@
 package de.tech26.supermarket.domain.usecase
 
 import arrow.core.right
+import io.mockk.every
+import io.mockk.mockk
+import java.math.BigDecimal
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.math.BigDecimal
 
 internal class CheckoutUseCaseTest {
 
-    private val checkout: CheckoutUseCase = CheckoutUseCase()
+    private val itemRepository: ItemRepository = mockk<ItemRepository>()
+    private val checkout: CheckoutUseCase = CheckoutUseCase(itemRepository)
 
     @Test
-    internal fun `should calculate checkout price for an empty cart`() {
+    internal fun `should not calculate anything when a cart is empty`() {
         val result = checkout(emptyList())
 
         assertThat(result).isEqualTo(BigDecimal.ZERO.right())
+    }
+
+    @Test
+    internal fun `should checkout for not empty cart`() {
+        every { itemRepository.getItemsByIds(setOf(Sku("A"), Sku("B"))) } returns listOf(
+            Item(Sku("A"), 2.0.toBigDecimal()),
+            Item(Sku("B"), 4.0.toBigDecimal())
+        )
+        val result = checkout(listOf("A", "B", "A"))
+
+        assertThat(result).isEqualTo(BigDecimal(8.0).right())
     }
 }
