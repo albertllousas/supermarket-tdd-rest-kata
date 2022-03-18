333ae1ea4a1da9c3ab33fd57c059dc0352476e3c
reinhard.seiler <reinhard.seiler@n26.com>
2022-03-18 15:02:08 +0100

    FINISHED! :D

--- src/main/kotlin/de/tech26/supermarket/infrastructure/driven/StaticItemRepository.kt
+++ src/main/kotlin/de/tech26/supermarket/infrastructure/driven/StaticItemRepository.kt
@@ -3,22 +3,21 @@ package de.tech26.supermarket.infrastructure.driven
 import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
-import arrow.core.sequenceEither
 import de.tech26.supermarket.domain.ItemsNotFoundError
 import de.tech26.supermarket.domain.model.Item
 import de.tech26.supermarket.domain.model.ItemRepository
 import de.tech26.supermarket.domain.model.Sku
+import org.springframework.stereotype.Repository
 
+@Repository
 object StaticItemRepository : ItemRepository {
 
-    private val itemsBySku = mapOf(
-        Sku("A") to Item(Sku("A"), 2.0.toBigDecimal()),
-        Sku("B") to Item(Sku("B"), 4.0.toBigDecimal()),
-    )
+    private val itemsBySku = listOf(
+        Item(Sku("A"), 2.0.toBigDecimal()),
+        Item(Sku("B"), 4.0.toBigDecimal()),
+    ).associateBy { it.sku }
 
-    override fun getItemsByIds(skus: List<Sku>): Either<ItemsNotFoundError, List<Item>> {
-        return skus
-            .map { itemsBySku[it]?.right() ?: ItemsNotFoundError(listOf(it.id)).left() }
-            .sequenceEither()
-    }
+    override fun getItemsByIds(skus: List<Sku>): Either<ItemsNotFoundError, List<Item>> =
+        if (skus.all { it in itemsBySku.keys }) skus.map { itemsBySku[it]!! }.right()
+        else ItemsNotFoundError(skus.filterNot { it in itemsBySku.keys }.map(Sku::id)).left()
 }
