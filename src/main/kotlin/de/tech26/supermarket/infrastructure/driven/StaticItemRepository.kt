d9c8b14882fbc16dd227473a2cf1b28476ffcdc0
iakov.burtsev <iakov.burtsev@n26.com>
2022-03-18 14:53:34 +0100

    Implement StaticItemRepository.kt

--- src/main/kotlin/de/tech26/supermarket/infrastructure/driven/StaticItemRepository.kt
+++ src/main/kotlin/de/tech26/supermarket/infrastructure/driven/StaticItemRepository.kt
@@ -1,4 +1,24 @@
 package de.tech26.supermarket.infrastructure.driven
 
-class StaticItemRepository {
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import arrow.core.sequenceEither
+import de.tech26.supermarket.domain.ItemsNotFoundError
+import de.tech26.supermarket.domain.model.Item
+import de.tech26.supermarket.domain.model.ItemRepository
+import de.tech26.supermarket.domain.model.Sku
+
+object StaticItemRepository : ItemRepository {
+
+    private val itemsBySku = mapOf(
+        Sku("A") to Item(Sku("A"), 2.0.toBigDecimal()),
+        Sku("B") to Item(Sku("B"), 4.0.toBigDecimal()),
+    )
+
+    override fun getItemsByIds(skus: List<Sku>): Either<ItemsNotFoundError, List<Item>> {
+        return skus
+            .map { itemsBySku[it]?.right() ?: ItemsNotFoundError(listOf(it.id)).left() }
+            .sequenceEither()
+    }
 }
--- src/test/kotlin/de/tech26/supermarket/infrastructure/driver/StaticItemRepositoryTest.kt
+++ src/test/kotlin/de/tech26/supermarket/infrastructure/driver/StaticItemRepositoryTest.kt
@@ -1,6 +1,8 @@
 package de.tech26.supermarket.infrastructure.driver
 
+import arrow.core.left
 import arrow.core.right
+import de.tech26.supermarket.domain.ItemsNotFoundError
 import de.tech26.supermarket.domain.model.Item
 import de.tech26.supermarket.domain.model.Sku
 import de.tech26.supermarket.infrastructure.driven.StaticItemRepository
@@ -8,10 +10,18 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class StaticItemRepositoryTest {
+
     @Test
     internal fun `should find items`() {
         val result = StaticItemRepository.getItemsByIds(listOf(Sku("A")))
 
         assertThat(result).isEqualTo(listOf(Item(Sku("A"), 2.0.toBigDecimal())).right())
+    }
+
+    @Test
+    internal fun `should fail when items not found`() {
+        val result = StaticItemRepository.getItemsByIds(listOf(Sku("A"), Sku("D"), Sku("C")))
+
+        assertThat(result).isEqualTo(ItemsNotFoundError(listOf("D", "C")).left())
     }
 }
